18fcea61744f94288d279eaf6a7b878fc89a5c4f
Jonathan Arana <jonathan@primermaster.net>
2020-02-17 23:15:19 -0600

    migrations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,19 +4,16 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-        DB::table('users')->insert([
-            'name' => 'admin',
-            'email' => 'admin@utzin.edu.mx',
-            'password' => bcrypt('admin'),
-            'roll' => '1',
-        ]);
-    }
+  /**
+  * Seed the application's database.
+  *
+  * @return void
+  */
+  public function run()
+  {
+    $this->call(UsuariosSeeder::class);
+    $this->call(MateriasSeeder::class);
+    $this->call(RelacionSeeder::class);
+
+  }
 }
